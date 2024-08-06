408df3b72e2eaaf5f761fdf21579d40bf0a81c0a
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-08-06 09:20:25 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,18 @@
+package Numbers;
+
+public class LCM {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 7;
+		
+		int lcm = (a > b) ? a : b;
+		
+		while(true){
+			if(lcm % a == 0 && lcm % b == 0){
+				System.out.println("LCM of "+a+" & "+b+" is "+lcm);
+				break;
+			}
+			lcm++;
+		}
+	}
+}
