3305a404adca4bc36ce1b4918fc31d0535210a59
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-08-08 09:22:16 +0530

    Add files via upload

---
+++ PrimeFactors.java
@@ -0,0 +1,28 @@
+package Numbers;
+import java.util.Scanner;
+
+public class PrimeFactors {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter any positive integer :: ");
+		int num = Integer.parseInt(scanner.nextLine().trim());
+		
+		System.out.println("The Prime Factors are :");
+		printPrimeFactors(num);
+		
+		scanner.close();
+	}
+	
+	private static void printPrimeFactors(int num){
+		int limit = (int) Math.sqrt(num);
+		
+		for(int i=2;i<=limit;i++){
+			while(num % i == 0){
+				System.out.print(i+", ");
+				num = num/i;
+			}
+		}
+		if(num > 2)
+			System.out.print(num);
+	}
+}
